f9550e4d6bef7b992ed16975273124ad68b12233
Tim Beeckmans <tim@onyxone.com>
2023-04-21 12:32:50 +0200

    Support multiple products on an invoice

--- makeInvoice.test.ts
+++ makeInvoice.test.ts
@@ -1,21 +1,47 @@
 import each from "jest-each";
 
-function createInvoice(productName: string, amount: number, unitPrice: number) {
-    return {
-        getLines : function () {
-            return [{
-                'productName': productName,
-                'amount': amount,
-                'price': unitPrice,
-            }]
-        },
-        getVat : function () {
-            return amount * unitPrice * 0.21;
-        },
-        getTotal : function () {
-            return (amount * unitPrice) + this.getVat();
-        }
+class Product {
+    constructor (
+        public productName: string,
+        public amount: number,
+        public unitPrice: number,
+    ) {
     }
+
+    public getTotal() {
+        return this.amount * this.unitPrice;
+    }
+}
+
+const VAT = 0.21;
+
+class Invoice {
+    constructor (
+        public products: Product[]
+    ) {
+    }
+
+    public getLines () {
+        return this.products.map(product => ({
+            'productName': product.productName,
+            'amount': product.amount,
+            'price': product.unitPrice,
+        }))
+    }
+    public getVat () {
+        return this.getTotalBeforeVat() * VAT;
+    }
+    private getTotalBeforeVat () {
+        return this.products.reduce((total, product) =>
+            total + product.getTotal(), 0);
+    }
+    public getTotal () {
+        return this.getTotalBeforeVat() + this.getVat();
+    }
+}
+
+function createInvoice(products: Product[]) {
+    return new Invoice(products);
 }
 
 describe("Creating an invoice", () => {
@@ -31,16 +57,28 @@ describe("Creating an invoice", () => {
         // given
 
         // when
-        var invoice = createInvoice("Pink filament", 2, 10.0);
+        var invoice = createInvoice([
+            new Product(
+                "Pink filament", 
+                2, 
+                10.0
+            )
+        ]);
 
         // then
-        checkInvoiceInfo(invoice, "Pink filament", 1, 20.0, 4.2, 24.2);
+        checkInvoiceInfo(invoice, "Pink filament", 2, 10.0, 4.2, 24.2);
     })
     test("with a single product with another price and name", () => {
         // given
 
         // when
-        var invoice = createInvoice("Green filament", 1, 20.0);
+        var invoice = createInvoice([
+            new Product(
+                "Green filament", 
+                1, 
+                20.0
+            )
+        ]);
 
         // then
         checkInvoiceInfo(invoice, "Green filament", 1, 20.0, 4.2, 24.2);
@@ -50,8 +88,16 @@ describe("Creating an invoice", () => {
 
         // when
         var invoice = createInvoice([
-            {"name": "Pink filament", "amount": 2, "unitPrice": 10.0},
-            {"name": "Green filament", "amount": 1, "unitPrice": 20.0},
+            new Product(
+                "Pink filament", 
+                2, 
+                10.0
+            ),
+            new Product(
+                "Green filament", 
+                1, 
+                20.0
+            ),
         ]);
 
         // then
@@ -63,5 +109,17 @@ describe("Creating an invoice", () => {
         expect(invoice.getLines()[1].price).toBe(20);
         expect(invoice.getVat()).toBe(8.4)
         expect(invoice.getTotal()).toBe(48.4)
+    });
+    
+    test("with an invoice number", () => {
+        var invoice = createInvoice([
+            new Product(
+                "Pink filament", 
+                2, 
+                10.0
+            )
+        ]);
+
+        expect(invoice.getInvoiceNumber()).not.toBeUndefined();
     });
 })
