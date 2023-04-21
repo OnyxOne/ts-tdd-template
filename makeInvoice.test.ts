444b82058987eb8243c09d86c3e6aa45395ca91f
Lennert <lennert@onyx.be>
2023-04-21 11:35:07 +0200

    initial commit

--- makeInvoice.test.ts
+++ makeInvoice.test.ts
@@ -1,17 +1,13 @@
 import each from "jest-each";
 
 describe("Test stuff", () => {
-    each([
-        true,
-        false
-    ]).test('runs it', (value) => {
-
+    each([true, false]).test('runs it', (value) => {
         expect(value).toEqual(true)
-    })
+    });
     test("test true", () => {
         expect(true).toBe(true)
-    })
+    });
     test("test false", () => {
         expect(false).toBe(true)
-    })
-})
+    });
+});
