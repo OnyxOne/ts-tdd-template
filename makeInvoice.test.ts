92cdd185dddc7f5aeb2a1baf40293a09682711f1
Lennert <lennert@onyx.be>
2023-04-21 11:37:54 +0200

    add more semicolons

--- makeInvoice.test.ts
+++ makeInvoice.test.ts
@@ -2,12 +2,12 @@ import each from "jest-each";
 
 describe("Test stuff", () => {
     each([true, false]).test('runs it', (value) => {
-        expect(value).toEqual(true)
+        expect(value).toEqual(true);
     });
     test("test true", () => {
-        expect(true).toBe(true)
+        expect(true).toBe(true);
     });
     test("test false", () => {
-        expect(false).toBe(true)
+        expect(false).toBe(true);
     });
 });
